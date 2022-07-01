b3a013efc666776bdd8f4c12b3195c2db926fa24
James Hagood <cardfaux@tutanota.com>
2022-06-30 20:08:04 -0500

    first commit with a nodemon.json file and a basic shader being used

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    IS_DEVELOPMENT: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  parser: '@babel/eslint-parser',
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'no-unused-vars': [2],
+  },
+};
